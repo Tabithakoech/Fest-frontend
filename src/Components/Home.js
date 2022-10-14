50f4babcf9e6b5886c5f1b5635dc28099c03f77f
TabithaKoech <tabbyk1718@gmail.com>
2022-10-14 10:53:47 +0300

    Created home components with the logo

---
+++ src/Components/Home.js
@@ -0,0 +1,23 @@
+// import reactRouterDom from 'react-router-dom';
+import React from 'react';
+// import './App.css';
+
+function Home() {
+
+  return (
+    
+    <div class="home">
+
+          <div>
+            <h1>Never miss a concert or live show again</h1>
+        
+        </div>
+
+      <div class="home-img">
+          {/* <img src="/concertpix/pexels-thibault-trillet-167491.jpg"></img> */}
+      </div>
+  </div>
+  );
+}
+
+export default Home;
